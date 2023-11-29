c8130a1ad174b8077dc4cafe8efd02f86b8cd9f1
subramanyam-kamath <124177206+subramanyam-kamath@users.noreply.github.com>
2023-11-29 12:40:36 +0530

    Update force_push_restricted_branches.sh

--- pre-receive-hooks /force_push_restricted_branches.sh
+++ pre-receive-hooks /force_push_restricted_branches.sh
@@ -1,40 +1,28 @@
 #!/usr/bin/env bash
 
+#
+# Pre-receive hook that will block any new commits that their names contain
+# other than lower-case alphabet characters (a-z).
+#
+# More details on pre-receive hooks and how to apply them can be found on
+# https://help.github.com/enterprise/admin/guides/developer-workflow/managing-pre-receive-hooks-on-the-github-enterprise-appliance/
+#
+
 zero_commit="0000000000000000000000000000000000000000"
 
-# This example allows force pushes for branches named scratch/* and test/*
-force_push_prefix="
-scratch
-test
-"
+# Ensure that [a-z] means only lower case ASCII characters => set LC_COLLATE to 'C'
+# See http://unix.stackexchange.com/questions/227070/why-does-a-z-match-lowercase-letters-in-bash
+# See https://www.gnu.org/software/bash/manual/bashref.html#Pattern-Matching
+LC_COLLATE='C'
 
-is_force_push() {
-  # If this is a new branch there's no history to overwrite
-  if [[ ${oldrev} == ${zero_commit} ]]; then
-    return 1
-  fi
-
-  if git merge-base --is-ancestor ${oldrev} ${newrev}; then
-    return 1
-  else
-    return 0
-  fi
-}
-
-while read -r oldrev newrev refname; do
-  if is_force_push; then
-    force_push_permitted=false
-    for push_prefix in ${force_push_prefix}; do
-      if [[ ${refname} == "refs/heads/${push_prefix}/"* ]]; then
-        force_push_permitted=true
-        break
-      fi
-    done
-    if [[ ${force_push_permitted} == true ]]; then
-      continue
-    else
-      echo "force push detected in restricted branch ${refname}"
+while read oldrev newrev refname; do
+  # Only check new branches ($oldrev is zero commit), don't block tags
+  if [[ $oldrev == $zero_commit && $refname =~ ^refs/heads/ ]]; then
+    # Check if the branch name is lower case characters (ASCII only), '-', '_', "/" or numbers
+    if [[ ! $refname =~ ^refs/heads/[-a-z0-9_/]+$ ]]; then
+      echo "Blocking creation of new branch $refname because it must only contain lower-case alpha-numeric characters, '-', '_' or '/'."
       exit 1
     fi
   fi
 done
+exit 0
